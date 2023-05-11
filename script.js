146cb2de48d5db62be931825f24f811e889d7c9f
Davi <werdavid1@gmail.com>
2023-05-11 09:26:03 -0300

    Classe jogador feita

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //window.alert("Bem-vindo");
+class Jogador{
+    nome;
+    poder;
+    vilao;
+    lugar;
+}
+
+nome = window.prompt("Digite seu nome");
+
+frase = document.getElementsByClassName("frase");
